de7d51422e937e510f41930e1c7e13e8aa684bf9
seven mike <sevenmike64@gmail.com>
2024-11-05 11:11:21 +0300

    add password check

--- main.py
+++ main.py
@@ -2,5 +2,7 @@ import config
 def hello(name, age):
     print(f'Hello, {name}!')
     print(f'Are you {age} y.o, right?')
+    data = input("Input your password: ")
+    print(data == config.password)
 
 hello(config.name, config.age)
